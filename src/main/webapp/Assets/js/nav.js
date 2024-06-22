808dd7ed275c4d2e3b63788c85aa8c11cfc120e5
yue24413 <2312400876@qq.com>
2024-06-22 23:02:27 +0800

    ks

--- src/main/webapp/Assets/js/nav.js
+++ src/main/webapp/Assets/js/nav.js
@@ -1,20 +1,16 @@
-$(document).ready(function () {
+$(function (){
     //判断是否存在ul
     $(".wc-menu li:has(ul)").hover(function () {
-        //$(this) 代表.navmenu li
         $(this).children("a").addClass("move");
         if ($(this).find("li").length > 0) {
             //显示下拉菜单
             $(this).children("div").stop(true, false).show();
         }
-    }, function () {
+    }, function () {    // 鼠标移出就执行回调函数
         $(this).children("a").removeClass("move");
-        //隐藏下拉菜单
         $(this).children("div").stop(true, false).hide();
     });
 });
-
-
 
 if ($(".right-nr table").length > 0) {
     $.each($(".right-nr table"), function (k, v) {
@@ -23,35 +19,25 @@ if ($(".right-nr table").length > 0) {
     });
 }
 
-
-
-
-var w = $(window).width();
+let w = $(window).width();
 if (w > 991) {
     $(".side-nav-panel").show();
 }
-
 $(window).resize(function () {
     w = $(window).width();
     if (w > 991) {
         $(".side-nav-panel").show();
     }
-
 });
 
 $(".nav_name").click(function () {
     $(".side-nav-panel").slideToggle(150);
 });
 
-
-
-$(document).ready(function () {
-    //判断是否存在ul
+$(function () {
     $(".ny-nav-ul li:has(ul)").hover(function () {
-        //$(this) 代表.navmenu li
         $(this).children("a").addClass("move");
         if ($(this).find("li").length > 0) {
-            //显示下拉菜单
             $(this).children("ul").stop(true, true).slideDown("slow")
         }
     }, function () {
