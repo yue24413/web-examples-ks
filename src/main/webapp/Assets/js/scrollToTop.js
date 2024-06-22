d83d79ab2f5b5c6319db406c9ec0c9e9f3be2066
yue24413 <2312400876@qq.com>
2024-06-22 23:02:27 +0800

    ks

--- src/main/webapp/Assets/js/scrollToTop.js
+++ src/main/webapp/Assets/js/scrollToTop.js
@@ -20,7 +20,7 @@ var scrolltotop = {
     //startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
     //scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
     setting: { startline: 1, scrollto: 0, scrollduration: 500, fadeduration: [500, 100] },
-    controlHTML: '<img src="' + _base_url_ + '/ASSETS/img/istop.png" style="width:40px;" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
+    controlHTML: '<img src="' + _base_url_ + '/ASSET/img/istop.png" style="width:40px;" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
     controlattrs: { offsetx: 60, offsety: 60 }, //offset of control relative to right/ bottom of window corner
     anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
 
