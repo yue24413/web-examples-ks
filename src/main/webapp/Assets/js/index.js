def549b3be00b6f28f79e5e57eedd32878140f5e
yue24413 <2312400876@qq.com>
2024-06-23 12:53:39 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/webapp/Assets/js/index.js
+++ src/main/webapp/Assets/js/index.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    var window_w = $(window).width(); // Window Width
-    var window_h = $(window).height(); // Window Height
-    var window_s = $(window).scrollTop(); // Window Scroll Top
+$(function () {
+    let window_w = $(window).width(); // Window Width
+    let window_h = $(window).height(); // Window Height
+    let window_s = $(window).scrollTop(); // Window Scroll Top
     // On Resize
-    $(window).resize(function () {
+    $(window).resize(function () {//窗口大小改变
         window_w = $(window).width();
         window_h = $(window).height();
         window_s = $(window).scrollTop();
@@ -18,70 +18,109 @@ $(document).ready(function () {
         mouseOutDelay: 200,
         responsiveThreshold: 973,
     });
-
-    if ($(window).width() > 991) {
-        $(document).off('click.bs.dropdown.data-api');
-        clickOpen();
-    };
-
-    $(window).resize(function () {
-        if ($(window).width() > 991) {
-            $(document).off('click.bs.dropdown.data-api');
-            clickOpen();
-        };
-    });
-
-    if ($(window).width() < 992) {
-        clickOpenMible();
-    }
-
-    $(window).resize(function () {
-        if ($(window).width() < 992) {
-            clickOpenMible();
-        };
-    });
-
-    function clickOpen() {
-        $(".header-nav .dropdown-toggle").click(function () {
-            var url = $(this).data('href');
-            window.location.href = url;
-        });
-    }
-
-    function clickOpenMible() {
-        $(".header-nav .dropdown-toggle span").click(function () {
-            var url = $(this).parent('.dropdown-toggle').data('href');
-            window.location.href = url;
-        });
-    }
-
-    $(".header-top .menu-top li").hover(function () {
-        var hideBox = $(this).children(".img-box");
-        if (hideBox.css("display") == "none") {
-            hideBox.slideDown("300");
-        } else {
-            hideBox.slideUp("300");
-        }
-    });
-
+    // if ($(window).width() > 991) {
+    //     $(document).off('click.bs.dropdown.data-api');
+    //     clickOpen();
+    // };
+    // $(window).resize(function () {
+    //     if ($(window).width() > 991) {
+    //         $(document).off('click.bs.dropdown.data-api');
+    //         clickOpen();
+    //     };
+    // });
+    // if ($(window).width() < 992) {
+    //     clickOpenMible();
+    // }
+    // $(window).resize(function () {
+    //     if ($(window).width() < 992) {
+    //         clickOpenMible();
+    //     };
+    // });
+    //
+    // function clickOpen() {
+    //     $(".header-nav .dropdown-toggle").click(function () {
+    //         const url = $(this).data('href');
+    //         window.location.href = url;
+    //     });
+    // }
+    //
+    // function clickOpenMible() {
+    //     $(".header-nav .dropdown-toggle span").click(function () {
+    //         const url = $(this).parent('.dropdown-toggle').data('href');
+    //         window.location.href = url;
+    //     });
+    // }
+    //
+    // $(".header-top .menu-top li").hover(function () {
+    //     const hideBox = $(this).children(".img-box");
+    //     if (hideBox.css("display") == "none") {
+    //         hideBox.slideDown("300");
+    //     } else {
+    //         hideBox.slideUp("300");
+    //     }
+    // });
 
     // 点击显示隐藏
-    $(".slide-door .accordion-title").click(function () {
-        var hideBox = $(this).next(".accordion-content");
-        if (hideBox.css("display") == "none") {
-            hideBox.slideDown("300");
-            $(this).addClass('open');
-        } else {
-            hideBox.slideUp("300");
-            $(this).removeClass('open');
-        }
-    });
+    // $(".slide-door .accordion-title").click(function () {
+    //     const hideBox = $(this).next(".accordion-content");
+    //     if (hideBox.css("display") == "none") {
+    //         hideBox.slideDown("300");
+    //         $(this).addClass('open');
+    //     } else {
+    //         hideBox.slideUp("300");
+    //         $(this).removeClass('open');
+    //     }
+    // });
 
+//顶部搜索
+    search(); //搜索框
+    function search() {
+        $('.search-btn').click( function () {
+            if ($('.search-box').height() == 210) {
+                $('.search-box').animate({
+                    height: '0',
+                    opacity: '0',
+                })
+                $('header').animate({
+                    top: '0',
+                })
+            } else {
+                $('.search-box').animate({
+                    height: '210',
+                    opacity: '1',
+                })
+                $('header').animate({
+                    top: '210',
+                })
+            }
+        })
+        $(".search-box .close").on('click', function () {
+            $('.search-box').animate({
+                height: '0',
+                opacity: '0',
+            })
+            $('header').animate({
+                top: '0',
+            })
+        })
+        $("body>*").click(function(){
+            $('.search-box').click(() =>{return false})
+            if($('.search-box').height() == 210){
+                $('.search-box').animate({
+                    height:'0',
+                    opacity: '0',
+                })
+                $('header').animate({
+                    top:'0',
+                })
+            }
+        })
+    }
 
     //手机设备的简单适配
-    var treeMobile = $('.site-tree-mobile'),
+    const treeMobile = $('.site-tree-mobile'),
         shadeMobile = $('.site-mobile-shade');
-    var _body = $("body");
+    const _body = $("body");
     treeMobile.on('click', function () {
         if (!_body.hasClass('site-mobile')) {
             _body.addClass('site-mobile');
@@ -90,41 +129,36 @@ $(document).ready(function () {
             _body.removeClass('site-mobile');
         }
     });
-
-    var dropdownToggleIcon = $('.dropdown-toggle .fa');
-    var backIcon = $('.dropdown-menu .back');
-    var closeIcon = $('.closeIcon');
-    var closeIcon2 = $('.dropdown-menu .closeIcon');
+    const dropdownToggleIcon = $('.dropdown-toggle .fa');
+    const backIcon = $('.dropdown-menu .back');
+    const closeIcon = $('.closeIcon');
+    const closeIcon2 = $('.dropdown-menu .closeIcon');
     dropdownToggleIcon.on('click', function () {
-        var dropdownMenu = $(this).parent('.dropdown-toggle').next('.dropdown-menu');
-        if (!dropdownMenu.hasClass('show'))
-            dropdownMenu.addClass('show');
-        else
-            dropdownMenu.removeClass('show');
-        shadeMobile.on('click', function () {
+        const dropdownMenu = $(this).parent('.dropdown-toggle').next('.dropdown-menu');
+        !dropdownMenu.hasClass('show') ? dropdownMenu.addClass('show'): dropdownMenu.removeClass('show');
+        shadeMobile.click(function () {
             _body.removeClass('site-mobile');
         });
-        backIcon.on('click', function () {
+        backIcon.click( function () {
             $(this).parents('.dropdown-menu').removeClass('show');
         });
-        closeIcon2.on('click', function () {
+        closeIcon2.click( function () {
             dropdownMenu.removeClass('show');
         });
     });
-
-    shadeMobile.on('click', function () {
+    shadeMobile.click( function () {
         _body.removeClass('site-mobile');
     });
-    closeIcon.on('click', function () {
+    closeIcon.click(function () {
         _body.removeClass('site-mobile');
     });
-
+    //手机端--用户手指第一次接触屏幕时触发
     $('.site-mobile-shade,.dropdown-menu,.nav').on("touchstart", function (e) {
+        //提取触摸点坐标:
         startX = e.originalEvent.changedTouches[0].pageX,
             startY = e.originalEvent.changedTouches[0].pageY;
     });
-
-    $('.site-mobile-shade').on("touchend", function (e) {                    
+    $('.site-mobile-shade').on("touchend", function (e) {
         moveEndX = e.originalEvent.changedTouches[0].pageX,
             moveEndY = e.originalEvent.changedTouches[0].pageY,
             X = moveEndX - startX,
@@ -137,18 +171,16 @@ $(document).ready(function () {
     });
 
     // 固定导航
-    var win = $(window);
-    var sc = $(document);
-
+    const win = $(window);
+    const sc = $(document);
     // header
     if ($(window).width() > 991) {
-        var headerNav = $(".header-nav");
+        const headerNav = $(".header-nav");
         if (headerNav.length) {
             var headerNav_offsetTop = headerNav.position().top;
         }
-
         win.scroll(function () {
-            var scTop = sc.scrollTop();
+            const scTop = sc.scrollTop();
             if (sc.scrollTop() >= headerNav_offsetTop + 120) {
                 $(".header").addClass('header-fixed')
             } else if (sc.scrollTop() < headerNav_offsetTop + 120) {
@@ -159,7 +191,7 @@ $(document).ready(function () {
     $(window).scroll(function() {
     	if($(document).scrollTop()>100){
     		$(".head-left").addClass('head-left1');
-    		  
+
     	}else{
     		$(".head-left").removeClass('head-left1');
     	}
@@ -167,29 +199,26 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         if ($(window).width() > 991) {
-            var headerNav = $(".header-nav");
+            const headerNav = $(".header-nav");
             if (headerNav.length) {
-                var headerNav_offsetTop = headerNav.position().top;
+                const headerNav_offsetTop = headerNav.position().top;
             }
-
             win.scroll(function () {
-                var scTop = sc.scrollTop();
+                const scTop = sc.scrollTop();
                 if (sc.scrollTop() >= headerNav_offsetTop + 120) {
                     $(".header").addClass('header-fixed');
-                  
+
                 } else if (sc.scrollTop() < headerNav_offsetTop + 120) {
                     $(".header").removeClass('header-fixed');
-                   
                 }
-                
+
             });
         }
     });
-
     // 侧边栏导航 sidebar-nav
     // 有导航点击展开下级菜单
     $(".sidebar-nav .fa").click(function () {
-        var hideBox = $(this).next("ul");
+        const hideBox = $(this).next("ul");
         if (hideBox.css("display") == "none") {
             hideBox.slideDown("300");
             $(this).parents('li').addClass('active');
@@ -201,71 +230,63 @@ $(document).ready(function () {
         }
     });
 
-
-    // 标签页
+    // ***标签页
     var page_url;
-    //初始化URL参数  
+    //初始化URL参数
     InitUrlParms = function () {
         var args = new Object();
-        var query = location.search.substring(1);//获取查询串     
-        var pairs = query.split("&");//在逗号处断开     
+        var query = location.search.substring(1);//获取查询串
+        var pairs = query.split("&");//在逗号处断开
         for (var i = 0; i < pairs.length; i++) {
-            var pos = pairs[i].indexOf('=');//查找name=value     
-            if (pos == -1) continue;//如果没有找到就跳过     
-            var argname = pairs[i].substring(0, pos);//提取name     
-            var value = pairs[i].substring(pos + 1);//提取value     
-            args[argname] = decodeURIComponent(value);//存为属性     
+            var pos = pairs[i].indexOf('=');//查找name=value
+            if (pos == -1) continue;//如果没有找到就跳过
+            var argname = pairs[i].substring(0, pos);//提取name
+            var value = pairs[i].substring(pos + 1);//提取value
+            args[argname] = decodeURIComponent(value);//存为属性
         }
         page_url = args;
-    };//封装好的函数，获取从a.html传来的参数type  
-
+    };//封装好的函数，获取从a.html传来的参数type
     InitUrlParms();
 
-    $(".horizontal-tag .tabs li").on('click', function () {
+    $(".horizontal-tag .tabs li").click(function () {
         $(this).addClass('active').siblings().removeClass('active');
-        var _index = $(this).index();
+        const _index = $(this).index();
         $('.horizontal-tag .tab-content .tab-item').eq(_index).addClass('active').siblings().removeClass('active');
     })
-    if (page_url["type"] != null) {
-        var tabIndex = page_url["type"];
-    } else {
-        var tabIndex = 0;
-    }
+    let tabIndex;
+    ( page_url["type"] != null) ? tabIndex = page_url["type"] :tabIndex = 0  ;
     //显示指定的tab内容  
     $('.horizontal-tag .tab-content .tab-item').eq(tabIndex).addClass('active').siblings().removeClass('active');
     //tab选项卡高亮  
     $('.horizontal-tag .tabs li').eq(tabIndex).addClass('active').siblings().removeClass('active');
-
     //如果检测到了有embed就调用播放插件
-    if ($("embed").attr('src')) {
-        var html = '<iframe style="border:0; margin: 0 auto;" src="/play/index?url=' + $("embed").attr('src') + '" width=600 height=500></iframe>';
-        $('.video-player').html(html);
-    }
+    // if ($("embed").attr('src')) {
+    //     const html = '<iframe style="border:0; margin: 0 auto;" src="/play/index?url=' + $("embed").attr('src') + '" width=600 height=500></iframe>';
+    //     $('.video-player').html(html);
+    // }
 
 
     // sub-nav
     function Body() {
         setTimeout(function () {
-            var ul = ".sub-nav ul";
-            var wrapper = ".sub-nav";
+            const ul = ".sub-nav ul";
+            const wrapper = ".sub-nav";
 
-            var width = $(wrapper).width();
+            const width = $(wrapper).width();
 
-            var totalWidth = 0;
+            let totalWidth = 0;
             $(".sub-nav ul li").each(function (index) {
-                var current_width = parseInt($(this).width(), 10);
+                const current_width = parseInt($(this).width(), 10);
                 if (current_width > 0) {
                     totalWidth += current_width;
                 }
             });
-
             totalWidth += 40;
-
             // ul offset //
 
-            var ul_offset = $(ul).offset();
+            const ul_offset = $(ul).offset();
 
-            var width_differences = totalWidth - width;
+            const width_differences = totalWidth - width;
 
             if (width_differences > 0) {
                 $('.previous_slider').remove();
@@ -276,7 +297,7 @@ $(document).ready(function () {
                 $(ul).addClass('ul_margin');
 
                 $('.next_slider').click(function () {
-                    var ul_offset_next = $(ul).offset();
+                    const ul_offset_next = $(ul).offset();
 
                     if ((ul_offset_next.left + width_differences - ul_offset.left) < 96) {
                         $(ul).css({
@@ -288,8 +309,8 @@ $(document).ready(function () {
                         });
                     }
 
-                    var ul_css_left_temp = $(ul).css('left');
-                    var ul_css_left = ul_css_left_temp.substr(0, ul_css_left_temp.length - 2);
+                    const ul_css_left_temp = $(ul).css('left');
+                    const ul_css_left = ul_css_left_temp.substr(0, ul_css_left_temp.length - 2);
 
                 });
 
@@ -315,7 +336,6 @@ $(document).ready(function () {
     }
 
     Body();
-
     $(window).resize(function () {
         if ($('.sub-nav').length > 0) {
             $(".sub-nav ul").css({
@@ -324,85 +344,6 @@ $(document).ready(function () {
             Body();
         }
     });
-    
-    //顶部搜索
-       search(); //搜索框
-    function search(){
-        $('.search-btn').on('click',function(){
-            $('.search-box').animate({
-                height:'210',
-                opacity: '1',
-            })
-            $('header').animate({
-                top:'210',
-            })
-        })
-        $(".search-box .close").on('click',function(){
-            $('.search-box').animate({
-                height:'0',
-                opacity: '0',
-            })
-            $('header').animate({
-                top:'0',
-            })
-        })
-        $("body>*").not('.search-box').on('click',function(){
-            if($('.search-box').height() == 210){
-                $('.search-box').animate({
-                    height:'0',
-                    opacity: '0',
-                })
-                $('header').animate({
-                    top:'0',
-                })
-            }
-        })
-    }
-    
-    //顶部搜索
-    search(); //搜索框
-    function search() {
-        $('.search-btn').on('click', function () {
-            if ($('.search-box').height() == 210) {
-                $('.search-box').animate({
-                    height: '0',
-                    opacity: '0',
-                })
-                $('header').animate({
-                    top: '0',
-                })
-            }
-            else {
-                $('.search-box').animate({
-                    height: '210',
-                    opacity: '1',
-                })
-                $('header').animate({
-                    top: '210',
-                })
-            }
-        })
-        $(".search-box .close").on('click', function () {
-            $('.search-box').animate({
-                height: '0',
-                opacity: '0',
-            })
-            $('header').animate({
-                top: '0',
-            })
-        })
-        //$("body>*").not('.search-box').on('click', function () {
-        //    if ($('.search-box').height() == 240) {
-        //        $('.search-box').animate({
-        //            height: '0',
-        //            opacity: '0',
-        //        })
-        //        $('header').animate({
-        //            top: '0',
-        //        })
-        //    }
-        //})
-    }
-    
+
 });
 
--- src/main/webapp/Assets/js/scrollToTop.js
+++ src/main/webapp/Assets/js/scrollToTop.js
@@ -2,26 +2,27 @@
 //** Available/ usage terms at http://www.dynamicdrive.com (March 30th, 09')
 //** v1.1 (April 7th, 09'):
 //** 1) Adds ability to scroll to an absolute position (from top of page) or specific element on the page instead.
-//** 2) Fixes scroll animation not working in Opera. 
+//** 2) Fixes scroll animation not working in Opera.
 
-
-var _base_url_;
-var _script_list_ = document.getElementsByTagName('script');
-for (var i = 0; i < _script_list_.length; i++) {
-    var _item_src_ = _script_list_[i].src;
+let _base_url_;
+const _script_list_ = document.getElementsByTagName('script');
+for (let i = 0; i < _script_list_.length; i++) {
+    let _item_src_ = _script_list_[i].src;
     if (_item_src_ && _item_src_.toUpperCase().indexOf('/ASSETS') > 0) {
         _base_url_ = _item_src_.substring(0, _item_src_.toUpperCase().indexOf('/ASSETS'));
+        console.log(_base_url_) //http://localhost:63342/web-exanples-ks/src/main/webapp
         break;
     }
 }
 
+//源  http://www.dynamicdrive.com/dynamicindex3/scrolltop.html
 var scrolltotop = {
 
     //startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
     //scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
-    setting: { startline: 1, scrollto: 0, scrollduration: 500, fadeduration: [500, 100] },
+    setting: { startline: 100, scrollto: 0, scrollduration: 500, fadeduration: [500, 100] },
     controlHTML: '<img src="' + _base_url_ + '/ASSET/img/istop.png" style="width:40px;" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
-    controlattrs: { offsetx: 60, offsety: 60 }, //offset of control relative to right/ bottom of window corner
+    controlattrs: { offsetx: 50, offsety: 50 }, //offset of control relative to right/ bottom of window corner
     anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
 
     state: { isvisible: false, shouldvisible: false },
